d83410b31984c59a1b0abcee47320176286e864b
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 15:54:20 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,5 @@
 
 module.exports = async (client) => {
-    status = cycle([
-    client.config.presence, 'github.com/jmesfo0'
-])
-
-  setInterval(client.user.setPresence(next(status)), 5000);
+  setInterval(client.user.setPresence(client.config.presence), 5000);
   console.log("Successfully Logged in as " + client.user.name);
 };
