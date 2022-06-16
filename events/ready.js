7e962e414845fceaedbba37bfa5a825085dd3a4d
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 01:18:55 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 module.exports = async (client) => {
-  client.manager.init(client.user.id);
   client.user.setPresence(client.config.presence);
-  client.log("Successfully Logged in as " + client.user.tag);
+  console.log("Successfully Logged in as " + client.user.name);
 };
