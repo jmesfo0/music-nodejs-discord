05ac48bf24da1a9ff4c1f1920e97ee4e30c02b16
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 01:00:11 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,11 @@
 module.exports = async (client) => {
+  (client.Ready = true),
+    client.user.setPresence({
+    status: client.config.Presence.status, // You can show online, idle, and dnd
+      activity: {
+        name: client.config.Presence.name,
+        type: client.config.Presence.type,
+      },
+});
     console.log(`${client.user.username} Login!`);
-
-    client.user.setActivity(client.config.listening);
 };
