5adf082d3342d927645691a9dfd5feb88599fd2c
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 01:11:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 module.exports = {
         px: '-',
-        Presence: {
-                status: "online", // You can show online, idle, and dnd
-                name: ".play", // The message shown
-                type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
-        },
+        presence: {
+    //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+    status: "online", // You can have online, idle, and dnd(invisible too but it make people think the bot is offline)
+    activities: [
+      {
+        name: "-play", //Status Text
+        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+      },
+    ],
+  },
 
     opt: {
         DJ: {
