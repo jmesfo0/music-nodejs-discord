46f2f354ecf5cb5ac0bbd033b3f33847001cfeb0
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 19:35:25 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,19 @@
 module.exports = async (client) => {
-  
+
   var items = ['Yes', 'No', 'Maybe'];
   var item = items[Math.floor(Math.random() * items.length)];
   
   client.user.setStatus('idle').catch(console.error);
     setInterval(() => {
-      client.user.setPresence(item);
+      client.user.setPresence(
+    //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+    status: "online", // You can have online, idle, and dnd(invisible too but it make people think the bot is offline)
+    activities: [
+      {
+        name: item, //Status Text
+        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+      },
+    ]);
                       }, 5000);
   
   
