f02779266d511ceb00e773429e01e19788040344
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 18:59:38 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ var items = ['Yes', 'No', 'Maybe'];
 var item = items[Math.floor(Math.random() * items.length)];
 
 module.exports = async (client) => {
-  setInterval(function () {client.user.setPresence(item);}, 5000);
-  console.log("Successfully Logged in as " + client.user.name);
+  setInterval(client.user.setPresence(item), 5000);
+  console.log("Successfully Logged in as " + client.user.tag);
 };
