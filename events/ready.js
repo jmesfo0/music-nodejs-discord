3e01e679a85740826ad0805f757e6fae0ae2b359
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 19:42:22 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,14 @@ module.exports = async (client) => {
 
   var items = ['Yes', 'No', 'Maybe'];
   var item = items[Math.floor(Math.random() * items.length)];
-  
-  client.user.setStatus('idle').catch(console.error);
-    setInterval(() => {
-      client.user.setPresence({
+      
+  setInterval(() => {client.user.setPresence({
         status: "online",  // You can show online, idle... Do not disturb is dnd
         game: {
-            name: item,  // The message shown
+            name: "!help",  // The message shown
             type: "PLAYING" // PLAYING, WATCHING, LISTENING, STREAMING,
         }
-    });
-                      }, 5000);
+    })}, 5000);
   
   
   console.log("Successfully Logged in as " + client.user.tag);
