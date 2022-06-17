9b680039089e8fe9aa4a3d27469a3e79509e4d88
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 19:51:05 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,4 @@
 
 module.exports = async (client) => {
-    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-    const activities_list = [
-    "-play", 
-    "tires screaching", 
-    "people argueing", 
-    "something", 
-    "your thoughts", 
-    ];
-setInterval(() => {
-        client.user.setActivity(activities_list[index], {type: 'LISTENING'});
-    }, 1000);
   console.log("Successfully Logged in as " + client.user.tag);
 };
