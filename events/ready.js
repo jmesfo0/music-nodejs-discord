b4762cf03046841183b1a5a94c30e22583d7b1f1
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 19:47:55 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,12 @@
 
 module.exports = async (client) => {
+    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
     const activities_list = [
     "-play", 
     "your mom", 
     "your grandma", 
     ];
 setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
         client.user.setActivity(activities_list[index], {type: 'WATCHING'});
     }, 1000);
   console.log("Successfully Logged in as " + client.user.tag);
