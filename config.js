28f9893d3ac2cd52c6f0dbbccf3d8b602ec4b8e8
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 19:32:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -18,33 +18,13 @@ module.exports = {
             }
         }
     },
-        presence1: {
+        presence: {
     //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
     status: "online", // You can have online, idle, and dnd(invisible too but it make people think the bot is offline)
     activities: [
       {
         name: "-play", //Status Text
         type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
-      },
-    ],
-  },
-        presence2: {
-    //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-    status: "online", // You can have online, idle, and dnd(invisible too but it make people think the bot is offline)
-    activities: [
-      {
-        name: "-play", //Status Text
-        type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING
-      },
-    ],
-  },
-        presence3: {
-    //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-    status: "online", // You can have online, idle, and dnd(invisible too but it make people think the bot is offline)
-    activities: [
-      {
-        name: "-play", //Status Text
-        type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING
       },
     ],
   }
