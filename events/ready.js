c82456c95abedaf610c36a3d171d0c892f261be5
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-16 19:24:35 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,17 @@
-var items = ['Yes', 'No', 'Maybe'];
-var item = items[Math.floor(Math.random() * items.length)];
 
-
-
-function myTimer() {
-  client.user.setPresence(client.config.presence(item));
-}
+var used1 = false;
+var version = '1.0.0';
 
 module.exports = async (client) => {
-  client.user.setPresence(client.config.presence);
-  setInterval(myTimer, 1000);
+  
+  var items = ['Yes', 'No', 'Maybe'];
+  var item = items[Math.floor(Math.random() * items.length)];
+  
+  client.user.setStatus('idle').catch(console.error);
+    setInterval(() => {
+      client.user.setPresence(item);
+                      }, 5000);
+  
+  
   console.log("Successfully Logged in as " + client.user.tag);
 };
